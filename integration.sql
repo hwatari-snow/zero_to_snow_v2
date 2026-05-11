3724855c8cce2b4e1c65ac0dbd324f0649e25f8b
hiroki watari <hiroki.watari@snowflake.com>
2026-05-11 22:39:29 +0900

    Update integration.sql

--- integration.sql
+++ integration.sql
@@ -6,18 +6,10 @@ CREATE OR REPLACE API INTEGRATION zero_to_snow_git_api
   API_ALLOWED_PREFIXES = ('https://github.com/hwatari-snow')
   ENABLED = TRUE;
 
--- 2. Git Repository を作成（tb_101 内に配置）
-USE DATABASE tb_101;
+-- 2. Git Repository を作成（learning 内に配置）
+USE DATABASE snowflake_learning_db;
+create schema public;
 
-CREATE OR REPLACE GIT REPOSITORY tb_101.public.zero_to_snow_v2
+CREATE OR REPLACE GIT REPOSITORY snowflake_learning_db.public.zero_to_snow_v2
   API_INTEGRATION = zero_to_snow_git_api
   ORIGIN = 'https://github.com/hwatari-snow/Zero_to_Snow_v2.git';
-
--- 3. リモートからフェッチ
-ALTER GIT REPOSITORY tb_101.public.zero_to_snow_v2 FETCH;
-
--- 4. 中身を確認
-SHOW GIT BRANCHES IN tb_101.public.zero_to_snow_v2;
-
--- ファイル一覧（mainブランチ）
-LS @tb_101.public.zero_to_snow_v2/branches/main/;
